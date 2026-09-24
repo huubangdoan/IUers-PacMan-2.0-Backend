2cafc11380f74ec5521ae9c4408f3126e6c8620e
tranghuyen1409-eng <tranghuyen1409@gmail.com>
2026-09-24 23:12:05 +0700

    add JWT

---
+++ Pacman/src/main/java/com/Iuer/Pacman/entity/User.java
@@ -0,0 +1,34 @@
+
+package com.Iuer.Pacman.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role = "USER";
+}
---
+++ Pacman/src/main/java/com/Iuer/Pacman/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.Iuer.Pacman.repository;
+
+import com.Iuer.Pacman.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+}

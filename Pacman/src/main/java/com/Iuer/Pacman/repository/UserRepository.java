d1f7024f3711bebaf73848025464e2cbd3926cf4
Huu Bang Doan <chocotochan@gmail.com>
2026-09-25 10:31:56 +0700

    fix: remove json run on old folder

--- Pacman/src/main/java/com/Iuer/Pacman/repository/UserRepository.java
+++ Pacman/src/main/java/com/Iuer/Pacman/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.Iuer.Pacman.repository;
 
-import com.Iuer.Pacman.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Iuer.Pacman.entity.User;
+
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(String username); // có thể xài Search
     boolean existsByUsername(String username);   
 }
--- Pacman/src/test/java/com/Iuer/Pacman/PacmanApplicationTests.java
+++ Pacman/src/test/java/com/Iuer/Pacman/PacmanApplicationTests.java
@@ -1,4 +1,4 @@
-package Iuer.example.Pacman;
+package com.Iuer.Pacman;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
